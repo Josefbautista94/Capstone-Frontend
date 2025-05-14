feb48856f1f849ee954c03fc33eea7f54e109b69
Jose <josefbautista94@gmail.com>
2025-05-14 00:30:48 -0400

    Fetch live NYC crime data with filtering and ordering via SODA API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BookmarksPage from "./pages/BookmarksPage/BookmarksPage";
+import Nav from "./components/Navbar/Navbar";
+import CrimeDataPage from "./pages/CrimeDataPage/CrimeDataPage";
 
 function App() {
   return (
     <Router>
+      <Nav />
       <Routes>
         <Route path="/bookmarks" element={<BookmarksPage />} />
+        <Route path="/crime" element={<CrimeDataPage />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
- 
--- src/api/nycCrimeApi.js
+++ src/api/nycCrimeApi.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const nycCrimeApi = axios.create({
-    baseURL : "https://data.cityofnewyork.us/resource/qb7u-rbmr.json",
-});
+    baseURL: "https://data.cityofnewyork.us/resource/5uac-w243.json",
+    headers: {
+      "X-App-Token": import.meta.env.VITE_APP_NYC_TOKEN
+    }
+  });
+  
 
 export default nycCrimeApi;
