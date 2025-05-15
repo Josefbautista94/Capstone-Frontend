9cd6b654944e5e4782772c0285397c4fa1cc4167
Jose <josefbautista94@gmail.com>
2025-05-15 00:54:32 -0400

    Starting on the implmentation of the InsightsPage

---
+++ src/pages/InsightsPage/InsightsPage.jsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import nycCrimeApi from "../../api/nycCrimeApi";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid } from "recharts";
+
+
+export default function InsightsPage() {
+    
+}
